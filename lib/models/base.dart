cf4c8ee917ccfd4188f5232309eb7ae26842a61e
venir.dev <luca@venir.dev>
2022-10-28 19:05:12 +0200

    Called my models inside main, this time.

--- lib/models/base.dart
+++ lib/models/base.dart
@@ -1,8 +1,6 @@
 import 'package:dart_mappable/dart_mappable.dart';
 import 'package:freezed_problems/main.mapper.g.dart';
 
-/// Uncomment the code above and below for a possible version of the convertere
-
 @MappableClass()
 abstract class Base with BaseMixin, Mappable {
   const Base();
